68b1e5ad85bce36c8783dd5999bc394111349606
osunaluis <luisosunac@gmail.com>
2024-03-27 18:19:32 -0700

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,6 +24,8 @@ set -x
 echo $PATH
 set +x
 
+sleep 5
+
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
