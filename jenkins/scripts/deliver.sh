235070f5f4aa5436fc5b0864cc5a848b488280a5
osunaluis <luisosunac@gmail.com>
2024-03-27 17:47:57 -0700

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -21,10 +21,10 @@ VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
 set +x
 
 set -x
-pwd
+PATH=$(pwd)
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+java -jar ${PATH}/target/${NAME}-${VERSION}.jar
